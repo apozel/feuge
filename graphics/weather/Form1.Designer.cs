dde598f30fd5a491cc9930ba1bd6647b2c816fea
apozel <52205094+apozel@users.noreply.github.com>
2020-04-12 19:53:17 +0200

    new graphics

---
+++ graphics/weather/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.latitudeLabel = new System.Windows.Forms.Label();
+            this.longitudeLabel = new System.Windows.Forms.Label();
+            this.villeLabel = new System.Windows.Forms.Label();
+            this.longitudeTextBox = new System.Windows.Forms.TextBox();
+            this.latitudeTextBox = new System.Windows.Forms.TextBox();
+            this.villeTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // latitudeLabel
+            // 
+            this.latitudeLabel.AutoSize = true;
+            this.latitudeLabel.Location = new System.Drawing.Point(108, 43);
+            this.latitudeLabel.Name = "latitudeLabel";
+            this.latitudeLabel.Size = new System.Drawing.Size(50, 13);
+            this.latitudeLabel.TabIndex = 0;
+            this.latitudeLabel.Text = "latitude : ";
+            // 
+            // longitudeLabel
+            // 
+            this.longitudeLabel.AutoSize = true;
+            this.longitudeLabel.Location = new System.Drawing.Point(111, 78);
+            this.longitudeLabel.Name = "longitudeLabel";
+            this.longitudeLabel.Size = new System.Drawing.Size(59, 13);
+            this.longitudeLabel.TabIndex = 1;
+            this.longitudeLabel.Text = "longitude : ";
+            // 
+            // villeLabel
+            // 
+            this.villeLabel.AutoSize = true;
+            this.villeLabel.Location = new System.Drawing.Point(111, 118);
+            this.villeLabel.Name = "villeLabel";
+            this.villeLabel.Size = new System.Drawing.Size(34, 13);
+            this.villeLabel.TabIndex = 2;
+            this.villeLabel.Text = "ville : ";
+            // 
+            // longitudeTextBox
+            // 
+            this.longitudeTextBox.Location = new System.Drawing.Point(202, 71);
+            this.longitudeTextBox.Name = "longitudeTextBox";
+            this.longitudeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.longitudeTextBox.TabIndex = 3;
+            // 
+            // latitudeTextBox
+            // 
+            this.latitudeTextBox.Location = new System.Drawing.Point(202, 36);
+            this.latitudeTextBox.Name = "latitudeTextBox";
+            this.latitudeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.latitudeTextBox.TabIndex = 4;
+            // 
+            // villeTextBox
+            // 
+            this.villeTextBox.Location = new System.Drawing.Point(202, 118);
+            this.villeTextBox.Name = "villeTextBox";
+            this.villeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.villeTextBox.TabIndex = 5;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(202, 186);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "lancer recherche";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(202, 271);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(100, 20);
+            this.resultTextBox.TabIndex = 7;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(111, 277);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(38, 13);
+            this.resultLabel.TabIndex = 8;
+            this.resultLabel.Text = "result :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.villeTextBox);
+            this.Controls.Add(this.latitudeTextBox);
+            this.Controls.Add(this.longitudeTextBox);
+            this.Controls.Add(this.villeLabel);
+            this.Controls.Add(this.longitudeLabel);
+            this.Controls.Add(this.latitudeLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label latitudeLabel;
+        private System.Windows.Forms.Label longitudeLabel;
+        private System.Windows.Forms.Label villeLabel;
+        private System.Windows.Forms.TextBox longitudeTextBox;
+        private System.Windows.Forms.TextBox latitudeTextBox;
+        private System.Windows.Forms.TextBox villeTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ graphics/weather/Form1.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace weather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private String IdWeather = "327f93f0117f1f0009c3a14ee69ff283";
+        private String idGeonames = "heleneCollavizza";
+        private WebClient Client = new WebClient();
+        private WeatherInfo info;
+
+       
+        private void changementInfo()
+        {
+            resultTextBox.Text = info.ToString();
+        }
+
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (latitudeTextBox.Text.Length != 0 && longitudeTextBox.Text.Length != 0)
+            {
+                string dataWeatherFromUserTown = null;
+                string dataGeonamesFromUserTown = null;
+                bool reussite = false;
+                try
+                {
+                    // if an exception is raise the program will ask for a town again
+                    dataWeatherFromUserTown = Client.DownloadString(String.Format("http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid={2}", latitudeTextBox.Text, longitudeTextBox.Text, IdWeather));
+                    dataGeonamesFromUserTown = Client.DownloadString(String.Format("http://api.geonames.org/timezoneJSON?&lat={0}&lng={1}&username={2}", latitudeTextBox.Text, longitudeTextBox.Text, idGeonames));
+                    Thread.Sleep(300);
+                    reussite = true;
+
+                }
+                catch (WebException ex)
+                {
+                    MessageBox.Show(string.Format("la ville n'existe pas ou est mal écrite : {0}.", ex.Message));
+                }
+                if (reussite)
+                {
+                    WeatherInfo testDayDuration = JsonConvert.DeserializeObject<WeatherInfo>(dataGeonamesFromUserTown);
+                    this.info = testDayDuration;
+                    changementInfo();
+                }
+
+            }
+            else if (villeTextBox.Text.Length != 0)
+            {
+
+                string urlTownOpenWeather = String.Format("http://api.openweathermap.org/data/2.5/weather?q={0},fr&appid={1}", villeTextBox.Text, IdWeather);
+                string rechercheVille = null;
+                try
+                {
+                    // if an exception is raise the program will ask for a town again
+                    rechercheVille = Client.DownloadString(urlTownOpenWeather);
+                    Thread.Sleep(300);
+                    MessageBox.Show(rechercheVille);
+
+                }
+                catch (WebException ex)
+                {
+                    MessageBox.Show(string.Format("la ville n'existe pas ou est mal écrite : {0}.", ex.Message));
+
+                }
+
+                if (rechercheVille.Length != 0)
+                {
+                    JObject weatherUserTownJson = JObject.Parse(rechercheVille);
+
+
+                    string dataGeonamesFromUserTown = null;
+
+                    try
+                    {
+                        // if an exception is raise the program will ask for a town again
+
+                        dataGeonamesFromUserTown = Client.DownloadString(String.Format("http://api.geonames.org/timezoneJSON?&lat={0}&lng={1}&username={2}", double.Parse(weatherUserTownJson.SelectToken("coord.lat").ToString()).ToString(CultureInfo.InvariantCulture), double.Parse(weatherUserTownJson.SelectToken("coord.lon").ToString()).ToString(CultureInfo.InvariantCulture), idGeonames));
+                        Thread.Sleep(300);
+
+
+
+
+                    }
+                    catch (WebException ex)
+                    {
+                        Console.WriteLine("la ville n'existe pas ou est mal écrite : {0}.", ex.Message);
+                    }
+                    if (dataGeonamesFromUserTown.Length != 0)
+                    {
+                        WeatherInfo testDayDuration = JsonConvert.DeserializeObject<WeatherInfo>(dataGeonamesFromUserTown);
+                        this.info = testDayDuration;
+
+                        changementInfo();
+
+                    }
+                }
+
+
+            }
+
+        }
+
+       
+    }
+}
